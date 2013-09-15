bee63689c92a7f465c625c22fe7b8eff29648d51
Nikita Kitaygora <see.minor@gmail.com>
2013-09-15 13:32:16 -0400

    simple login

---
+++ javascripts/main.js
@@ -0,0 +1,24 @@
+var ref = new Firebase('https://bi-reader.firebaseio.com/');
+
+var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+  if (error) {
+    // an error occurred while attempting login
+    console.log(error);
+  } else if (user) {
+    // user authenticated with Firebase
+    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+    location.hash = user.firebaseAuthToken;
+  } else {
+    // user is logged out
+  }
+});
+
+var logmein = function() {
+	auth.login('password', {
+		email: 'smack.it@gmail.com',
+		password: 'smackit'
+	});
+}
+
+var h3 = document.getElementById("login");
+h3.onclick = logmein;
