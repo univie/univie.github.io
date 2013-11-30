66f4542bd286abd89460385417bb522cc0037a39
Nikita Kitaygora <see.minor@gmail.com>
2013-11-30 14:31:48 -0500

    oh ok

--- javascripts/main.js
+++ javascripts/main.js
@@ -63,10 +63,9 @@ $(document).ready(function() {
     if (email.length === 0) {return alert('Please enter your email and try again.');}
     if (password.length === 0) {return alert('Please enter your password and try again.');}
     if (signup) {
-      if (passwordVerify !== password) {return alert('Passwords don\'t match.');}
-      
+      if (!(/.+@.+\..+/).test(email)) {alert('Please enter a valid email address.'); return;}
+      if (passwordVerify !== password) {alert('Passwords don\'t match.'); return;}
       auth.createUser(email, password, function(error, user) {
-        if (!(/.+@.+\..+/).test(email)) {alert('Please enter a valid email address.'); return;}
         console.log('User Id: ' + user.id + ', Email: ' + user.email);
         loginEmail(email, password, rememberMe);
       });
