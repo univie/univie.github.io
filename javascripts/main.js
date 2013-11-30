48eac65612809d67298dde094bd787325bd35fc3
Nikita Kitaygora <see.minor@gmail.com>
2013-11-30 14:10:14 -0500

    wtf

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,26 +13,19 @@ var auth = new FirebaseSimpleLogin(ref, function(error, user) {
   }
 });
 
+var signup;
 $(document).ready(function() {
-  var logmein = function(event) {
-    console.log('logmein');
-    auth.login('password', {
-      'email': document.getElementById("username").value,
-      'password': document.getElementById("password").value,
-      'rememberMe': true
-    });
-    event.preventDefault();
-    // return false;
-  };
   $('#passwordVerifyDiv').hide(); // hide password verify by default
 
   $('#signupButton').click(function() { // show password verify when singup button clicked
-    $('#nonEmailDiv').hide();
+    signup = true;
+    // $('#nonEmailDiv').hide();
     $('#passwordVerifyDiv').show();
     $('#submitButton').text('Sign up');
   });
   $('#loginButton').click(function() { // hide password verify when login button clicked
-    $('#nonEmailDiv').show();
+    signup = false;
+    // $('#nonEmailDiv').show();
     $('#passwordVerifyDiv').hide();
     $('#submitButton').text('Log in');
   });
@@ -47,12 +40,36 @@ $(document).ready(function() {
       'rememberMe': true/*,
       'oauth_token': '15952759-QCp4pH3tLaSF8L6e3wLDxvqlCfkpcNAAbNeHVWQFK',
       'oauth_token_secret': 'fm2RiZ2VY33Fsnx1XWMsISbjYHgnHML30NUsjAVAssm2V',*/
-      
+
     });
   });
 
-  $('#submitButton').click(function() {
-
+  $('#submitButton').click(function(event) {
+    var email = $('#inputEmail').val(),
+        password = $('#inputPassword').val(),
+        passwordVerify = $('#passwordVerify').val(),
+        rememberMe = $('#rememberMe').is(':checked'),
+        loginEmail = function(email, password, rememberMe) {
+          auth.login('password', {
+            'email': email,
+            'password': password,
+            'rememberMe': rememberMe
+          });
+        };
+    event.preventDefault();
+    if (email.length === 0) {return alert('Please enter your email and try again.');}
+    if (password.length === 0) {return alert('Please enter your password and try again.');}
+    if (signup) {
+      if (passwordVerify !== password) {return alert('Passwords don\'t match.');}
+      auth.createUser(email, password, function(error, user) {
+        if (!error) {
+          console.log('User Id: ' + user.id + ', Email: ' + user.email);
+          loginEmail(email, password, rememberMe);
+        }
+      });
+    } else {
+      loginEmail(email, password, rememberMe);
+    }
   });
   
 });
