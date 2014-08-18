4c31d05f8abf81a1a090176fd617cca3f89c5004
Nikita Kitaygora <see.minor@gmail.com>
2014-08-18 02:11:09 -0400

    fixed admin, deleted apk file

--- javascripts/admin.js
+++ javascripts/admin.js
@@ -1,8 +1,30 @@
 angular.module('LoginPage', [])
 .controller('AdminCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
-	var ref = new Firebase('https://bi-reader.firebaseio.com/users');
-	ref.on('value', function(usersSnapshot) {
-		console.log('value!');
-		console.log(userSnapshot.val());
+	window.addEventListener("message", function(event) {
+	    // We only accept messages from ourselves
+	    if (event.source != window)
+	      return;
+
+	    if (event.data.type && (event.data.type == "HERES_TOKEN")) {
+			var authToken = event.data.token,
+				ref = new Firebase('https://bi-reader.firebaseio.com/users');
+			ref.auth(authToken);
+			ref.once('value', function(usersSnapshot) {
+				$scope.users = usersSnapshot.val();
+				$scope.$apply();
+				console.log($scope.users);
+			});
+		}
+		$scope.toggleActive = function(uid) {
+			var authinfo = $scope.users[uid].authinfo;
+			if (authinfo.pending) {
+				authinfo.pending = null;
+				authinfo.active = true;
+			} else {
+				authinfo.active = !authinfo.active;				
+			}
+			ref.child(uid).child('authinfo').set(authinfo);
+		};
 	});
+
 }]);
